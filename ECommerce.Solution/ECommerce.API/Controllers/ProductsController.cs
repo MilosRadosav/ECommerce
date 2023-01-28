764bb104bccfb4e71956d8b9e4b7f71ad61a697f
Milos <misa90etf@gmail.com>
2023-01-28 21:02:21 +0100

    InitialCommit

---
+++ ECommerce.Solution/ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using ECommerce.Core.Core.Data;
+using ECommerce.Core.Core.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreDbContext _storeDbContext;
+
+        public ProductsController(StoreDbContext storeDbContext)
+        {
+            _storeDbContext = storeDbContext;
+        }
+        [HttpGet("get-products")]
+        public async Task<List<Product>> GetProducts() 
+        {
+            var result = await _storeDbContext.Products.ToListAsync();
+
+            return  result;
+        }
+
+        [HttpGet("get-product-by-id/{id}")]
+        public async Task<Product> GetProductById(int id)
+        {
+           return await _storeDbContext.Products.FirstOrDefaultAsync(x=>x.Id==id);
+        }
+
+        [HttpPost("insert-product")]
+        public async Task<Product> InserProduct([FromBody] Product product)
+        {
+             await _storeDbContext.Products.AddAsync(product);
+             await _storeDbContext.SaveChangesAsync();
+
+            var result = await _storeDbContext.Products.FirstOrDefaultAsync(x=> x.Name==product.Name);
+
+            return result;
+        }
+
+        [HttpPut("modify-product")]
+        public async Task<Product> UpdateProduct([FromBody] Product product)
+        {
+            _storeDbContext.Products.Update(product);
+            await _storeDbContext.SaveChangesAsync();
+
+            var result = await _storeDbContext.Products.FirstOrDefaultAsync(x => x.Name == product.Name);
+
+            return result;
+        }
+
+
+        [HttpDelete("delete-product/{Id}")]
+        public async Task<string> UpdateProduct(int Id)
+        {
+            var removeProduct = await _storeDbContext.Products.FirstOrDefaultAsync(x => x.Id == Id);
+            var result = _storeDbContext.Products.Remove(removeProduct);
+            await _storeDbContext.SaveChangesAsync();
+
+
+            return "Deleted";
+        }
+    }
+}
---
+++ ECommerce.Solution/Infrastructure/Data/StoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Core.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Core.Core.Data
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
