f33148b2f5fe2a0b2607ce67f2817483e25681b0
Milos <misa90etf@gmail.com>
2023-02-17 23:32:03 +0100

    End of section 8b

--- EComerce-ui/ecommerce/src/app/app.component.ts
+++ EComerce-ui/ecommerce/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecommerce';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Pagination<Product[]>>(
+        'https://localhost:7067/api/products/get-products?pageSize=50'
+      )
+      .subscribe({
+        next: (response) => (this.products = response.data),
+        error: (error) => console.log(error),
+        complete: () => {
+          console.log('request completed');
+        },
+      });
+  }
 }
