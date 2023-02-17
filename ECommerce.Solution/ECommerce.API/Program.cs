9b12c5ddd5d4fd2628c75b616a037df4fe232bdf
Milos <misa90etf@gmail.com>
2023-02-17 23:32:55 +0100

    asdasd

--- ECommerce.Solution/ECommerce.API/Program.cs
+++ ECommerce.Solution/ECommerce.API/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors();
 
 
 var app = builder.Build();
@@ -28,7 +29,11 @@ app.UseStatusCodePagesWithReExecute("/errors/{0}");
     app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseCors();
+app.UseCors(x => x
+ .AllowAnyOrigin()
+ .AllowAnyMethod()
+ .AllowAnyHeader()
+ );
 
 app.UseAuthorization();
 
